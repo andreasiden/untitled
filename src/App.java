7fc20ddb3dcfac51b20bf3c1fce0454f3ad17065
andreasiden <andreasiden1@gmail.com>
2018-04-18 02:28:03 +0200

    test2

--- src/App.java
+++ src/App.java
@@ -1,11 +1,9 @@
 import javafx.application.Application;
-import javafx.beans.property.StringProperty;
 import javafx.fxml.FXMLLoader;
 import java.sql.SQLException;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import java.sql.SQLException;
 
 
 public class App extends Application {
@@ -20,7 +18,7 @@ public class App extends Application {
             e.printStackTrace();
         }
 
-        ModelTwo model = new ModelTwo(conn);
+        Model model = new Model(conn);
         launch(args);
     }
 
